1a277eff90c6bd15ad4321b36cbcba0857a814f1
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-02-20 20:27:04 +0000

    Exercise 2 – Create User Profile Card Component. Completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileCardComponent } from './user-profile-card/user-profile-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'user-profile';
